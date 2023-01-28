347c31f56528eb6fbe3a9c663ef44d87fdddef64
Dariaaal <1406dariaal@gmail.com>
2023-01-28 19:04:50 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
-import { debounce } from 'lodash.debounce';
-import countriesTpl from './fetchCountries';
-import {Notify} from 'notiflix';
+import debounce from 'lodash.debounce';
+import countries from './fetchCountries';
+import Notiflix from 'notiflix';
 
 const inputEl = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
@@ -9,14 +9,25 @@ const countryInfoCard = document.querySelector('.country-info')
 
 const DEBOUNCE_DELAY = 300;
 
-inputEl.addEventListener('input', _.debounce(onInputChange, DEBOUNCE_DELAY));
+inputEl.addEventListener('input', debounce(onInputChange,DEBOUNCE_DELAY));
 
 function onInputChange(e){
 e.preventDefault();
 
 const searchQuery = e.target.value;
 
-fetchCountries(searchQuery).then
+countries.fetchCountries(searchQuery)
+.then(renderCountryCard)
+.catch(onFetchError)
+.finally(()=>inputEl.reset());
 };
 
+function renderCountryCard(country){
+    const markup = countries(country);
+    inputEl.innerHTML = markup;
+};
+
+function onFetchError(error){
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+}
 
