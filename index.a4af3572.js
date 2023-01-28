951f16d2d6c7be784662ab0158fc0fc0122bb9b6
Dariaaal <111464521+Dariaaal@users.noreply.github.com>
2023-01-28 11:37:21 +0000

    Deploying to gh-pages from @ Dariaaal/goit-js-hw-10@13a8134846fd0a58e9f43cd061f133ccbcdb605b 🚀

---
+++ index.a4af3572.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#search-box");document.querySelector(".country-list"),document.querySelector(".country-info");e.addEventListener("input",(function(e){e.preventDefault();var t=e.target.value;fetchCountries(t).then}))}();
+//# sourceMappingURL=index.a4af3572.js.map
